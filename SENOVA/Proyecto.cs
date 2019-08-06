54165fe47edaaacc188ed1dba9f4ef777de5ef7d
unknown <Sala300@TUNDFPCGE10F372.FormacionSena.RED>
2019-08-06 15:06:03 -0500

    Initial commit

---
+++ SENOVA/Proyecto.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SENOVA
+{
+    public class Proyecto
+    {
+        #region Atributos
+        #endregion
+        #region Propiedades
+        public string Name { get; set; }
+        public double Code { get; set; }
+        public string Area { get; set; }
+        public int Duration { get; set; }
+        #endregion
+        #region Contructores
+        public Proyecto(string nombre, double codigo, string area, int duracion)
+        {
+            Name = nombre;
+            Code = codigo;
+            Area = area;
+            Duration = duracion;
+        }
+        public Proyecto()
+        {
+
+        }
+        public Proyecto( string area, int duracion)
+        {
+       
+            Area = area;
+            Duration = duracion;
+        }
+        #endregion
+        #region Metodos
+        public void IngresarProyecto()
+        {
+            Console.WriteLine("Ins¿grese el nombre delproyecto");
+            Name = Console.ReadLine();
+            Console.WriteLine("Ingrese el codigo");
+            Code = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el Area del proyecto");
+            Area = Console.ReadLine();
+            Console.WriteLine("Ingrese la duracion del proyecto");
+            var Dura = Console.ReadLine();
+            Duration = int.Parse(Dura);
+        }
+
+        public void ImprimirProyecto()
+        {
+            Console.WriteLine($"Name {Name}\n Code {Code}\n Area IP {Area}\n Duration {Duration}");
+        }
+        #endregion
+    }
+}
